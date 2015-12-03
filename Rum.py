771bbd0eae7c92c7fe2d302ce9e666e5cc2e22df
Gleydson Silva <gleydson404@gmail.com>
2015-12-03 20:20:02 -0200

    Inicio do desenvolvimento do arquvo Rum.

--- Rum.py
+++ Rum.py
@@ -6,6 +6,5 @@ from os.path import isfile, join
 
 files = [f for f in listdir(pds.PATH) if isfile(join(pds.PATH, f))]
 
-data = [pds.get_dataset(f for f in files)]
 
-print data
+data = [pds.get_dataset(pds.PATH+"/"+file) for file in files]
